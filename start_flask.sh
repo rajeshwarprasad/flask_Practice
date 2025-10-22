ca85974e7870fab05ed597d75a57eb22b9b5f211
mohanDevOps-arch <kandikuppa.krishna@herovired.com>
2025-10-22 18:03:05 +0530

    Update start_flask.sh to use sudo for installations

--- start_flask.sh
+++ start_flask.sh
@@ -10,11 +10,13 @@ fi
 source venv/bin/activate
 
 # Upgrade pip and install dependencies
-pip install --upgrade pip
-pip install -r requirements.txt black pylint bandit pytest pytest-html
+sudo apt update
+sudo apt install python3-pip -y
+sudo pip install --upgrade pip
+sudo pip install -r requirements.txt black pylint bandit pytest pytest-html
 
 # Ensure .env exists
 echo -e "MONGO_URI=mongodb+srv://mohan:Herovired123@herovired.f3do4.mongodb.net/studentDB\nSECRET_KEY=your-secret-key" > .env
 
 # Run app
-nohup python3 app.py --host=0.0.0.0 --port=5000 > flask.log 2>&1 &
+sudo nohup python3 app.py --host=0.0.0.0 --port=5000 > flask.log 2>&1 &
